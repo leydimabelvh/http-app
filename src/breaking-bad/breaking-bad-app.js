586bb2f3a460866b30844a9aa422f6d99c3e179e
Leydi Velazco <leydi.velazco@outlook.es>
2023-02-14 23:46:58 -0500

    Add quote rendering

--- src/breaking-bad/breaking-bad-app.js
+++ src/breaking-bad/breaking-bad-app.js
@@ -1,11 +1,25 @@
+import { fetchQuote } from "./fetch-api";
+
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-
 export const BreakingBadApp = async(element) => {
     document.querySelector('#title-app').innerHTML = 'Breaking Bad App';   
     
     element.innerHTML = 'Loading...';
+       
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h4');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'NEXT QUOTE';
+    
+    const renderQuote = (quote) => {
+        quoteLabel.innerHTML = quote.quote;
+        authorLabel.innerHTML = quote.author;
+        element.replaceChildren( quoteLabel, authorLabel, nextQuoteButton );  
+    }
 
+    fetchQuote()
+                .then( quote => renderQuote(quote) )
 }
